8b03b8ed5dcda6f5cda74ca4b4ec281b4e477ad7
mazfaa <kangazfa@gmail.com>
2024-09-27 12:32:49 +0700

    Career page frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "blog.html", "sustainability.html"],
+  content: ["index.html", "blog.html", "sustainability.html", "career.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "career-hero": 'url("/src/img/career-hero.jpg")',
+      },
+    },
   },
   plugins: [],
 };
